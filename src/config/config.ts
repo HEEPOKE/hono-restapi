6ccae5777ead357d40ad279d7595ef76f9009a23
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2023-08-14 02:33:05 +0700

    :money_with_wings: add prisma

---
+++ src/config/config.ts
@@ -0,0 +1,14 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const { DATABASE_URL, HOST, PORT, SWAGGER_URL } = process.env;
+
+const config = {
+    DATABASE_URL,
+    HOST,
+    PORT,
+    SWAGGER_URL,
+}
+
+export default config;
