c69de46f8985304e31ba9b53e231072e922df08e
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2023-08-15 15:24:33 +0700

    :money_with_wings: update controller

---
+++ src/controllers/account_controeller.ts
@@ -0,0 +1,41 @@
+import { Context } from "hono";
+import ConstantService from "../constants/services";
+import StatusCodeConstant from "../constants/code";
+import ResponseMessage from "../models/messages/response";
+import AccountServices from "../services/account_service";
+
+async function AccountListController(c: Context) {
+  try {
+    const accountList = await AccountServices.listAccount();
+
+    const response: ResponseMessage = {
+      status: {
+        code: StatusCodeConstant.SUCCESS.code,
+        message: StatusCodeConstant.SUCCESS.message,
+        service: ConstantService.ACCOUNT_SERVICE,
+        description: "Account list retrieved successfully",
+      },
+      data: accountList,
+    };
+
+    return c.json(response, 200);
+  } catch (err: any) {
+    const response: ResponseMessage = {
+      status: {
+        code: StatusCodeConstant.FAILED.code,
+        message: StatusCodeConstant.FAILED.message,
+        service: ConstantService.ACCOUNT_SERVICE,
+        description: `account list err :: ${err}`,
+      },
+      data: null,
+    };
+
+    return c.json(response, 500);
+  }
+}
+
+const AccountController = {
+  list: AccountListController,
+};
+
+export default AccountController;
